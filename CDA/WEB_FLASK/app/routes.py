1506e4d83a17695f0900ea4a48f6bd8aff9f5ba5
paimblancleo@orange.fr <paimblancleo@orange.fr>
2024-09-23 14:13:02 +0200

    Sace_13

--- CDA/WEB_FLASK/app/routes.py
+++ CDA/WEB_FLASK/app/routes.py
@@ -1,7 +1,11 @@
+from flask import render_template
 from app import app
 
 @app.route('/') # decorators
+def home():
+    return render_template('home.html')
+
 @app.route('/index')
 def index():
-    strResult = 'Hello Bruz!'
+    strResult = f'Hello Bruz! '
     return strResult
