c2abe9f720b7be21031b8a236638e96b3ecab9ad
paimblancleo@orange.fr <paimblancleo@orange.fr>
2024-09-24 12:17:57 +0200

    Save23

---
+++ CDA/Bdd.py
@@ -0,0 +1,27 @@
+import mysql.connector
+
+config = {#Infos concernant la base de donnée
+    'user': 'root',
+    'password': 'uimm',
+    'host': 'localhost',
+    'database': 'CDA'
+}
+
+
+try:
+    connexion = mysql.connector.connect(**config)# Connexion à la base de données
+
+    mycursor = connexion.cursor()# Création d'un curseur
+
+    mycursor.execute("show databaseS ") # Requette
+
+    for x in mycursor : # Ecriture des infos sur chaque lignes
+        print(x)
+    print ("================")
+
+except mysql.connector.Error as e: #Détection d'erreurs
+    print(f"Erreur : {e}")
+
+finally: # Fermeture du curseur et de la connexion
+    mycursor.close()
+    connexion.close()
