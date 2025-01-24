9a6473a37cd91600df8524afa4bf8afd1bedb445
Gracias2k16 <paimblancleo@orange.fr>
2025-01-24 10:20:06 +0100

    24_01_25_1021

--- CDA/WEB_FLASK/app/static/script.js
+++ CDA/WEB_FLASK/app/static/script.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     const h3Element = document.getElementById('Recherche_véhicule'); // Sélectionne le h3 par son ID
     const pElement = document.querySelector('.Recherche_véhicule_texte'); // Sélectionne le p avec la classe
+    const ulElement = document.getElementById('Dynamic_list');
 
     console.log('Le script est bien chargé et exécuté');
 
@@ -9,9 +10,12 @@ document.addEventListener("DOMContentLoaded", function() {
         
         if (pElement.style.display === 'none' || pElement.style.display === '') {
             pElement.style.display = 'block'; // Affiche le texte
+            ulElement.style.display = 'block'; // Affiche la liste
             console.log('Le texte a été affiché');
+
         } else {
             pElement.style.display = 'none'; // Cache le texte
+            ulElement.style.display = 'none'; // Cache la Liste
             console.log('Le texte a été caché');
         }
     });
