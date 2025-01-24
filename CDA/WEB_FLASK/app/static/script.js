1a17db3d145878106f8e5d5ea9426173b14c0035
Gracias2k16 <paimblancleo@orange.fr>
2025-01-24 10:53:18 +0100

    24_01_25_1053

--- CDA/WEB_FLASK/app/static/script.js
+++ CDA/WEB_FLASK/app/static/script.js
@@ -1,20 +1,23 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const h3Element = document.getElementById('Recherche_véhicule'); // Sélectionne le h3 par son ID
-    const pElement = document.querySelector('.Recherche_véhicule_texte'); // Sélectionne le p avec la classe
+    const h3Element = document.getElementById('Recherche_véhicule'); // Sélection par ID
+    const pElement1 = document.querySelector('.Recherche_véhicule_texte'); // Sélection par la classe
+    const pElement2 = document.querySelector('.Recherche_véhicule_texte_2'); // Sélection par la classe
     const ulElement = document.getElementById('Dynamic_list');
-
+    
     console.log('Le script est bien chargé et exécuté');
 
     h3Element.addEventListener('click', () => {
         console.log('Clic sur le h3 détecté');
         
-        if (pElement.style.display === 'none' || pElement.style.display === '') {
-            pElement.style.display = 'block'; // Affiche le texte
+        if (pElement1.style.display === 'none' || pElement1.style.display === '') {
+            pElement1.style.display = 'block'; // Affiche le texte
+            pElement2.style.display = 'block'; // Affiche le texte
             ulElement.style.display = 'block'; // Affiche la liste
             console.log('Le texte a été affiché');
 
         } else {
-            pElement.style.display = 'none'; // Cache le texte
+            pElement1.style.display = 'none'; // Cache le texte
+            pElement2.style.display = 'none'; // Cache le texte
             ulElement.style.display = 'none'; // Cache la Liste
             console.log('Le texte a été caché');
         }
