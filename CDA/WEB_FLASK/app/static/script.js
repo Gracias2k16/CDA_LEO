095fb33d407ec08fc5914487c83b4f7da87a0864
Gracias2k16 <paimblancleo@orange.fr>
2025-01-27 11:59:31 +0100

    27_01_25_1159

--- CDA/WEB_FLASK/app/static/script.js
+++ CDA/WEB_FLASK/app/static/script.js
@@ -1,4 +1,8 @@
-document.addEventListener("DOMContentLoaded", function() {
+/*//////////////////////////////////////////////////////////////////////
+/////////////-/////    Fonction affichage texte 1   //////////////////
+//////////////////////////////////////////////////////////////////////*/
+
+document.addEventListener("Fonction_texte_1", function() {
     const h3Element = document.getElementById('Recherche_véhicule'); // Sélection par ID
     const pElement1 = document.querySelector('.Recherche_véhicule_texte'); // Sélection par la classe
     const pElement2 = document.querySelector('.Recherche_véhicule_texte_2'); // Sélection par la classe
@@ -23,3 +27,28 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     });
 });
+
+/*//////////////////////////////////////////////////////////////////////
+/////////////-/////    Fonction affichage texte 2   //////////////////
+//////////////////////////////////////////////////////////////////////*/
+
+document.addEventListener("Fonction_texte_2", function() {
+    const h3Element = document.getElementById('controle_rapport'); // Sélection par ID
+    const pElement = document.querySelector('.controle_rapport_texte'); // Sélection par la classe
+
+    console.log('Le script est bien chargé et exécuté');
+
+    h3Element.addEventListener('click', () => {
+        console.log('Clic sur le h3 détecté');
+        
+        if (pElement.style.display === 'none' || pElement.style.display === '') {
+            pElement.style.display = 'block'; // Affiche le texte
+            console.log('Le texte a été affiché');
+
+        } else {
+            pElement.style.display = 'none'; // Cache le texte
+            console.log('Le texte a été caché');
+        }
+    });
+});
+
