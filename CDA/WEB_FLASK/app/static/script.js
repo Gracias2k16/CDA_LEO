045c72c1073312fb89682690e47da6fc99575e13
Gracias2k16 <paimblancleo@orange.fr>
2025-01-31 11:11:51 +0100

    31_01_25_1111

--- CDA/WEB_FLASK/app/static/script.js
+++ CDA/WEB_FLASK/app/static/script.js
@@ -173,3 +173,29 @@ document.addEventListener("DOMContentLoaded", () => {
     // Attacher les événements pour la première fois
     attachButtonEvents();
 });
+
+/*//////////////////////////////////////////////////////////////////////
+///////////////////////    Fonction menu glissant //////////////////////
+//////////////////////////////////////////////////////////////////////*/
+
+ // Sélection des éléments
+ const menu = document.getElementById("menuOverlay");
+ const openBtn = document.getElementById("Txt_menu");
+ const closeBtn = document.getElementById("closeMenu");
+
+ // Ouvrir le menu
+ openBtn.addEventListener("click", function() {
+     menu.classList.add("active");
+ });
+
+ // Fermer le menu
+ closeBtn.addEventListener("click", function() {
+     menu.classList.remove("active");
+ });
+
+ // Fermer en cliquant en dehors du menu
+ document.addEventListener("click", function(event) {
+     if (!menu.contains(event.target) && !openBtn.contains(event.target)) {
+         menu.classList.remove("active");
+     }
+ });
