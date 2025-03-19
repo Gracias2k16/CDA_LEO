8e76fc30a44b57ec8964a476063a7a9c8b65fa2d
Gracias2k16 <paimblancleo@orange.fr>
2025-03-19 16:36:27 +0100

    19_03_25_1636

--- CDA/WEB_FLASK/app/static/JS/Demande.js
+++ CDA/WEB_FLASK/app/static/JS/Demande.js
@@ -28,10 +28,40 @@
 ////////////////////    Fonction Selection moteur  //////////////////////
 //////////////////////////////////////////////////////////////////////*/
 
-let valeur = 0; // Initialisation de la variable
+let valeurMoteur = 0;
+    const boutons = document.querySelectorAll(".Choix_Moteur_Selection");
 
-    document.getElementById("Choix_Moteur_Selection").addEventListener("click", function() {
-        this.style.backgroundColor = "red"; // Changer la couleur
-        valeur = 1; // Mettre la variable à 1
-        console.log("Valeur =", valeur); // Afficher dans la console
+    boutons.forEach(bouton => {
+        bouton.addEventListener("click", function() {
+            // Supprimer la classe 'selected' de tous les boutons
+            boutons.forEach(btn => btn.classList.remove("selected"));
+
+            // Ajouter la classe 'selected' au bouton cliqué
+            this.classList.add("selected");
+
+            // Mettre à jour la variable valeur
+            valeurMoteur = this.getAttribute("data-value");
+            console.log("Valeur sélectionnée =", valeurMoteur);
+        });
+    });
+
+ /*//////////////////////////////////////////////////////////////////////
+////////////////////    Fonction Selection moteur  //////////////////////
+//////////////////////////////////////////////////////////////////////*/
+
+let valeurBoite = 0;
+    const boutonsBoite = document.querySelectorAll(".Choix_Boite_Selection");
+
+    boutonsBoite.forEach(bouton => {
+        bouton.addEventListener("click", function() {
+            // Supprimer la classe 'selected' de tous les boutons
+            boutonsBoite.forEach(btn => btn.classList.remove("selected2"));
+
+            // Ajouter la classe 'selected' au bouton cliqué
+            this.classList.add("selected2");
+
+            // Mettre à jour la variable valeur
+            valeurBoite = this.getAttribute("data-value");
+            console.log("Valeur sélectionnée =", valeurBoite);
+        });
     });
