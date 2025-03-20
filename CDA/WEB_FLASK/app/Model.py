8cd622f667eac9857c33ef479ed2c03891ae3409
Gracias2k16 <paimblancleo@orange.fr>
2025-03-20 09:47:16 +0100

    20_03_25_0947

--- CDA/WEB_FLASK/app/Model.py
+++ CDA/WEB_FLASK/app/Model.py
@@ -3,7 +3,8 @@ from app.Setting import Email_MDP, Email
 from app.__init__ import connexion_à_BDD
 import smtplib
 from email.message import EmailMessage
-from flask import request, flash, redirect, url_for
+from flask import request, flash, redirect, url_for, render_template, session
+from app.__init__ import bcrypt
 
 #===================================================================================================
 
@@ -99,9 +100,6 @@ def Recuperation_tous_utilisateurs():
     except mysql.connector.Error as err:
         print(f"Erreur lors de la récupération : {err}")
         return None
-    finally:
-        cur.close()
-        conn.close()
 
 #===================================================================================================
 
@@ -142,3 +140,76 @@ def gerer_comptes_Fonction():
         conn.close()
 
     return redirect(url_for('Comptes'))
+
+#===================================================================================================
+
+def Connexion_utilisateur():
+    if request.method == 'POST':
+        email = request.form['identifiant']
+        password = request.form['password']
+
+        user = Recupération_des_utilisateurs(email)  # Récupère l'utilisateur en BDD
+
+        if user and bcrypt.check_password_hash(user['id_Mdp'], password):  # Vérifie le mot de passe
+            session['user_id'] = user['id_Utilisateur']
+            session['email'] = user['id_Mail']
+            session['role'] = user['id_Type']
+            flash(f"Bienvenue, {user['id_Mail']} !", 'success')
+            return redirect(url_for('home'))
+        else:
+            flash("Identifiant ou mot de passe incorrect.", 'danger')
+
+    return render_template ('Connexion.html')
+
+#===================================================================================================
+
+def Création_Compte():
+    if request.method == 'GET':
+        return render_template('Creation_compte.html')  # Affiche la page HTML avec le formulaire
+
+    elif request.method == 'POST':
+        data = request.form  # Récupère les données envoyées par le formulaire
+
+    required_fields = ["Nom", "Mail","Mdp", "Mdp_2","Num"] # Champs requis
+    if not all(field in data and data[field] for field in required_fields):
+        flash("Certains champs obligatoires sont manquants.", 'danger')
+        return redirect(url_for('Creation_compte')) #Erreur si tous els champs ne sont pas compéltés
+
+    nom = data["Nom"]
+    prenom = data.get("Prenom", "")  # Optionnel
+    societe = data.get("Sociétée", "")  # Optionnel
+    mail = data["Mail"]
+    mot_de_passe = data["Mdp"]
+    mot_de_passe_confirmation = data["Mdp_2"]
+    num = data["Num"]
+
+    if mot_de_passe != mot_de_passe_confirmation:
+            flash("Les mots de passe ne correspondent pas.", 'danger')
+            return redirect(url_for('Creation_compte'))
+
+
+    hashed_password = bcrypt.generate_password_hash(mot_de_passe).decode('utf-8')
+
+    conn, cur = connexion_à_BDD()  # Connexion à la BDD
+    if conn is None or cur is None:
+        flash("Impossible de se connecter à la base de données.", 'danger')
+        return redirect(url_for('Creation_compte'))
+    
+    try:
+        cur.execute("SELECT * FROM Compte WHERE id_Mail = %s", (mail,))# Vérifier si l'email existe déjà
+        if cur.fetchone():
+            flash("Cet email est déjà utilisé.", 'danger')
+            return redirect(url_for('Creation_compte'))
+
+        sql = "INSERT INTO Compte (id_Nom, id_Prenom, id_Nom_societee, id_Mail, id_Mdp, Num_tel, id_Type) VALUES (%s, %s, %s, %s, %s, %s, %s)" #requete sql pour inseré le compte
+        cur.execute(sql, (nom, prenom, societe, mail, hashed_password, num, 'USER'))
+        conn.commit()
+
+        flash("Compte créé avec succès !", 'success')
+        Envoie_mail_confirmation(mail)
+        return redirect(url_for('Creation_compte'))
+
+    except mysql.connector.Error as e:
+        conn.rollback()
+        flash(f"Erreur lors de la création du compte : {str(e)}", 'danger')
+        return redirect(url_for('Creation_compte'))
--- CDA/WEB_FLASK/app/routes.py
+++ CDA/WEB_FLASK/app/routes.py
@@ -1,8 +1,7 @@
 from flask import request, render_template, flash, redirect, url_for, session
 import mysql.connector
 from app import app
-from CDA.WEB_FLASK.app.Model import connexion_à_BDD, Recupération_des_utilisateurs, Envoie_mail_confirmation, gerer_comptes_Fonction
-from app.__init__ import bcrypt
+from app.Model import connexion_à_BDD, gerer_comptes_Fonction, Connexion_utilisateur, Création_Compte
 import mysql
 from flask import jsonify
 
@@ -11,22 +10,7 @@ from flask import jsonify
 
 @app.route('/Connexion', methods=["GET", "POST"])
 def Connexion():
-    if request.method == 'POST':
-        email = request.form['identifiant']
-        password = request.form['password']
-
-        user = Recupération_des_utilisateurs(email)  # Récupère l'utilisateur en BDD
-
-        if user and bcrypt.check_password_hash(user['id_Mdp'], password):  # Vérifie le mot de passe
-            session['user_id'] = user['id_Utilisateur']
-            session['email'] = user['id_Mail']
-            session['role'] = user['id_Type']
-            flash(f"Bienvenue, {user['id_Mail']} !", 'success')
-            return redirect(url_for('home'))
-        else:
-            flash("Identifiant ou mot de passe incorrect.", 'danger')
-
-    return render_template ('Connexion.html')
+    return Connexion_utilisateur();
 
 #===================================================================================================
 
@@ -43,61 +27,9 @@ def Demande():
 #===================================================================================================
 
 @app.route('/Creation_compte', methods=['GET', 'POST'])
-def Creation_compte():
-    if request.method == 'GET':
-        return render_template('Creation_compte.html')  # Affiche la page HTML avec le formulaire
-
-    elif request.method == 'POST':
-        data = request.form  # Récupère les données envoyées par le formulaire
-
-    required_fields = ["Nom", "Mail","Mdp", "Mdp_2","Num"] # Champs requis
-    if not all(field in data and data[field] for field in required_fields):
-        flash("Certains champs obligatoires sont manquants.", 'danger')
-        return redirect(url_for('Creation_compte')) #Erreur si tous els champs ne sont pas compéltés
-
-    nom = data["Nom"]
-    prenom = data.get("Prenom", "")  # Optionnel
-    societe = data.get("Sociétée", "")  # Optionnel
-    mail = data["Mail"]
-    mot_de_passe = data["Mdp"]
-    mot_de_passe_confirmation = data["Mdp_2"]
-    num = data["Num"]
-
-    if mot_de_passe != mot_de_passe_confirmation:
-            flash("Les mots de passe ne correspondent pas.", 'danger')
-            return redirect(url_for('Creation_compte'))
-
-
-    hashed_password = bcrypt.generate_password_hash(mot_de_passe).decode('utf-8')
-
-    conn, cur = connexion_à_BDD()  # Connexion à la BDD
-    if conn is None or cur is None:
-        flash("Impossible de se connecter à la base de données.", 'danger')
-        return redirect(url_for('Creation_compte'))
+def Creation_compte_route():
+    return Création_Compte();
     
-    try:
-        cur.execute("SELECT * FROM Compte WHERE id_Mail = %s", (mail,))# Vérifier si l'email existe déjà
-        if cur.fetchone():
-            flash("Cet email est déjà utilisé.", 'danger')
-            return redirect(url_for('Creation_compte'))
-
-        sql = "INSERT INTO Compte (id_Nom, id_Prenom, id_Nom_societee, id_Mail, id_Mdp, Num_tel, id_Type) VALUES (%s, %s, %s, %s, %s, %s, %s)" #requete sql pour inseré le compte
-        cur.execute(sql, (nom, prenom, societe, mail, hashed_password, num, 'USER'))
-        conn.commit()
-
-        flash("Compte créé avec succès !", 'success')
-        Envoie_mail_confirmation(mail)
-        return redirect(url_for('Creation_compte'))
-
-    except mysql.connector.Error as e:
-        conn.rollback()
-        flash(f"Erreur lors de la création du compte : {str(e)}", 'danger')
-        return redirect(url_for('Creation_compte'))
-
-    finally:
-        cur.close()
-        conn.close()
-
 #===================================================================================================
 
 @app.route('/session-data')
