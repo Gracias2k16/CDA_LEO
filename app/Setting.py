5e91a05dc05f557bd1fe013d303c43eccf03611b
Gracias2k16 <paimblancleo@orange.fr>
2025-03-26 10:39:50 +0100

    test_déploiement_bdd_26_03_25_1040

--- app/Setting.py
+++ app/Setting.py
@@ -3,10 +3,14 @@ from dotenv  import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
 
+#//////////////////////////////  Gestion BDD   ////////////////////////////
+
 DB_HOST = os.getenv("DB_HOST")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_DATABASE = os.getenv("DB_DATABASE")
 DB_USERNAME = os.getenv("DB_USERNAME")
+
+#//////////////////////////////  Gestion Email   ////////////////////////////
 
 Email_MDP = os.getenv ("Adressemail_MDP")
 Email = os.getenv ("Adressemail")
