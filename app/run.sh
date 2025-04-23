71e1331fb9d9feb996cefa0219427ad72eec82ce
Gracias2k16 <paimblancleo@orange.fr>
2025-04-23 11:02:28 +0200

    Ajout run.sh 23_04_25_1102

---
+++ app/run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+
+# Active environnement virtuel
+source .venv/bin/activate
+
+# libére le port 5000 si déjà utilisé
+PORT=5000
+if lsof -i :$PORT >/dev/null; then
+  echo "Port $PORT déjà utilisé, on kill le process..."
+  kill $(lsof -t -i :$PORT)
+fi
+
+echo "Lancement de Flask..."
+python serverWEB.py
